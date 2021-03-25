d9c8d7bf5c8df5e36e689bd00076864c01a355ad
Elizabethpv <48952600+Elizabethpv@users.noreply.github.com>
2021-03-25 12:41:07 +0530

    Add project files.

---
+++ DataHandlingTempData/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataHandlingTempData.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult StudentView()
+        {
+            TempData["Name"] = "Anu";
+            TempData["Email"] = "anu@gmail.com";
+            TempData["DateOfBirth"] = "12-23-2020";
+            TempData["Place"] = "Piravom";
+            TempData["Malayalam"] = 34;
+            TempData["Maths"] =78;
+            TempData["English"] = 90;
+            return View();
+        }
+    }
+}
